23daa14f533bd6e20d1c80295ee176e458b0e5a1
Libo Li <liboli.lilibo@gmail.com>
2020-04-28 12:09:04 -0400

    comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,12 +6,18 @@ const cors = require('cors')({ origin: true });
 export const fetchToken = functions.https.onRequest(async (request, response) => {
     
     const url = 'https://developer.api.autodesk.com/authentication/v1/authenticate'
+    // Retrieve credentials from the environment
     const client_id = functions.config().forge.client_id
     const client_secret = functions.config().forge.secret_secret
+
+    // Only data:read scope should be needed. Check documentation
+    // for other scopes
     const scope = 'data:read'
 
+    // cors is added here
     cors(request, response, async () => {
         try {
+            // make the call
             const token = await axios({
                 url,
                 method: 'post',
@@ -23,8 +29,10 @@ export const fetchToken = functions.https.onRequest(async (request, response) =>
                 })
             })
             response.set('Access-Control-Allow-Origin', '*');
+            // return the token
             response.status(200).send(token)
         } catch (error) {
+            // return all errors as a 500 server error
             response.status(500).send(error)
         }
     })
